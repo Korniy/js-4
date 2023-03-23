7bfe945b856ea577d37c5abc682c694d76fddb3b
Volodymyr Korniychuk <korniychuk49@gmail.com>
2023-03-23 17:13:27 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const btnStrartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+let timerId = null;
+
+btnStrartEl.addEventListener('click', onButtonStart);
+btnStopEl.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  if (timerId) {
+    btnStrartEl.setAttribute('disabled', 'disabled');
+    btnStopEl.removeAttribute('disabled', 'disabled');
+  }
+}
+
+function onButtonStop() {
+  clearInterval(timerId);
+  if (timerId) {
+    btnStopEl.setAttribute('disabled', 'disabled');
+    btnStrartEl.removeAttribute('disabled', 'disabled');
+  }
+}
